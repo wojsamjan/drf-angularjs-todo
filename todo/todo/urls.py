b1491ef7a83f1badef78aae41f4661fa27c3d848
Wojciech Januszek <wojsamjan@gmail.com>
2018-05-01 00:19:51 +0200

    Added url for todo api

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -17,7 +17,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from todo.views import ToDoView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='todo/index.html'))
+    url(r'^$', TemplateView.as_view(template_name='todo/index.html')),
+    url(r'^todo/api/$', ToDoView.as_view())
 ]
