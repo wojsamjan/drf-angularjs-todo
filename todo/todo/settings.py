c8c66b911178ee14d669080a84d92a33694f4d42
Wojciech Januszek <wojsamjan@gmail.com>
2018-04-30 22:08:11 +0200

    Changed database sqlite3->mysql

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_todo',
+        'USER': 'devuser',
+        'PASSWORD': ''
     }
 }
 
