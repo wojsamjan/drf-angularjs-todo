91af17222f3bcb73ccc6a18f8c75a3cbcdae1576
Wojciech Januszek <wojsamjan@gmail.com>
2018-04-30 20:58:26 +0200

    Added script run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# . run.sh
+
+# set virtual environment
+source ./venv/bin/activate
+
+# export DJANGO_SETTINGS_MODULE & PYTHONPATH
+export DJANGO_SETTINGS_MODULE=todo.settings
+export PYTHONPATH=/Users/wjanuszek/Projects/PycharmProjects/goodridge/todo/todo
