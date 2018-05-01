a23d685755400615266dddd6b4309510ab7aceda
Wojciech Januszek <wojsamjan@gmail.com>
2018-05-02 00:43:29 +0200

    Updated saving and removing data

--- todo/static/js/todo.js
+++ todo/static/js/todo.js
@@ -2,19 +2,24 @@ var app = angular.module('toDo', []);
 app.controller('toDoController', function($scope, $http) {
   // $scope.todoList = [{todoText: 'Finish this app', done: false}];
 
-  $http.get('/todo/api/').then(function(response) {
+  function refresh(response) {
     $scope.todoList = [];
     for (var i = 0; i < response.data.length; i++) {
       var todo = {}
+      todo.id = response.data[i].id
       todo.todoText = response.data[i].text
       todo.done = response.data[i].done
       $scope.todoList.push(todo);
     }
-  });
+  };
+
+  $http.get('/todo/api/').then(refresh)
 
   $scope.saveData = function() {
     var data = {text: $scope.todoInput, done: false}
-    $http.put('/todo/api/', data)
+    $http.put('/todo/api/', data).then(function(response) {
+      $http.get('/todo/api/').then(refresh)
+    });
   };
 
   $scope.todoAdd = function() {
@@ -25,8 +30,12 @@ app.controller('toDoController', function($scope, $http) {
   $scope.remove = function() {
     var oldList = $scope.todoList;
     $scope.todoList = [];
-    angular.forEach(oldList, function(x) {
-      if (!x.done) $scope.todoList.push(x);
+    angular.forEach(oldList, function(todo) {
+      if (todo.done) {
+        $http.delete('/todo/api/' + todo.id + '/');
+      } else {
+        $scope.todoList.push(todo);
+      }
     });
   };
 });
